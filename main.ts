6c02ba56c1b21ba37887ed02e30c18e6ccb14206
gavin-gallant <gavin.gallant@mths.ca>
2024-10-15 11:23:21 -0400

    code shows light level

--- main.ts
+++ main.ts
@@ -1,8 +1,32 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Gavin Gallant
+ * Created on: Sep 2024
+ * This program uses the RobotBit's Neopixels
 */
 
-basic.showString('Hello, World!')
+// variables
+let neopixelStrip: neopixel.Strip = null
+
+// cleanup
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, () => {
+    let LightLevel = input.lightLevel()
+    basic.showNumber(LightLevel)
+    if(LightLevel<= 51)
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        if(LightLevel>52)
+            neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Orange))
+                if(LightLevel>104)
+                    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Yellow))
+                        if(LightLevel>208)
+                            neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+})
