74b5f2be19fc4bd893d26a8e26b3e6f1400f9f0f
gavin-gallant <gavin.gallant@mths.ca>
2024-10-16 11:25:17 -0400

    fixed code

--- main.ts
+++ main.ts
@@ -1,32 +1,44 @@
-/* Copyright (c) 2020 MTHS All rights reserved
- *
- * Created by: Gavin Gallant
- * Created on: Sep 2024
- * This program uses the RobotBit's Neopixels
-*/
+// Copyright (c) 2020 MTHS All rights reserved
+// Created by: Gavin Gallant
+// Created on: Sep 2024
+// This program uses the RobotBit's Neopixels
 
-// variables
-let neopixelStrip: neopixel.Strip = null
+// Variables
+let neopixelStrip: neopixel.Strip = null;
 
-// cleanup
-basic.clearScreen()
-neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.show()
-basic.showIcon(IconNames.Happy)
+// Cleanup
+basic.clearScreen();
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB);
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black));
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black));
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black));
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black));
+neopixelStrip.show();
+basic.showIcon(IconNames.Happy);
 
+// Button A pressed event
 input.onButtonPressed(Button.A, () => {
-    let LightLevel = input.lightLevel()
-    basic.showNumber(LightLevel)
-    if(LightLevel<= 51)
-        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
-        if(LightLevel>52)
-            neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Orange))
-                if(LightLevel>104)
-                    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Yellow))
-                        if(LightLevel>208)
-                            neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
-})
+    let lightLevel = input.lightLevel();
+    basic.showNumber(lightLevel);
+
+    // Reset all colors to black
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black));
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black));
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black));
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black));
+
+    if (lightLevel <= 51) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red));
+    }
+    if (lightLevel > 51 && lightLevel <= 104) {
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Orange));
+    }
+    if (lightLevel > 104 && lightLevel <= 208) {
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Yellow));
+    }
+    if (lightLevel > 208) {
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green));
+    }
+
+    neopixelStrip.show();
+});
